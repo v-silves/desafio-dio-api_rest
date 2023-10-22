35746c0ac2072fe3086e221619291e6a02ce7c3d
Victória Silvestre <143358954+v-silves@users.noreply.github.com>
2023-10-22 17:36:54 -0300

    Update and rename Lista.java to List.java
    Alterando modelo.

---
+++ src/main/list/model/List.java
@@ -0,0 +1,20 @@
+const listaCompras = {
+  user: [
+    {
+    "id": "string",
+    "name": "string",
+    "account": {
+        "id": numer,
+        "limit": 1000.00
+      },
+    "balance": 1000.00
+    }
+  ],
+  produts: [
+    {
+      "id": number,
+      "group": "string",
+      "quantity": number
+    }
+  ]
+}

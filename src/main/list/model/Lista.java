e5d0fb4163a514ab821a2e2aad1d9176ddbe52eb
Victória Silvestre <143358954+v-silves@users.noreply.github.com>
2023-10-22 16:46:44 -0300

    Create Lista.java
    Criando esquema dos dados utilizados na lista de compras.

---
+++ src/main/list/model/Lista.java
@@ -0,0 +1,14 @@
+{
+"name": "string",
+"account": "string" {
+  "limit": 200,
+  "balance": 1000.00
+},
+"produtos": [
+  {
+    "nome": "string",
+    "group": "string",
+    "quantidade": number
+  }
+]
+}
